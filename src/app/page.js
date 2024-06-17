512c70fd15ab2035dd4f5f73c4594f9797027659
Sricharan2k3 <ugs21117_cse.charan@cbit.org.in>
2024-06-17 20:00:11 +0530

    updated home page charan

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
-import Navbar from "./components/navbar";
+
 import Carousel from "./components/home/carousel";
+import FourCards from "./components/home/fourcards";
+import Collaborators from "./components/home/collaborators";
+import VideoContainer from "./components/home/videocontainer";
+import FarmMechanization from "./about/page";
+
 
 
 const slides = [
@@ -15,13 +20,19 @@ export default function Home() {
   return (
 
     <div>
-      <Navbar />
+
 
       <Carousel autoSlide={true} >
         {[...slides.map((s) => (
           <img src={s} key={s} />
         ))]}
       </Carousel>
+      <FarmMechanization />
+      <FourCards />
+      <Collaborators />
+      <VideoContainer />
+
+
     </div>
 
 
