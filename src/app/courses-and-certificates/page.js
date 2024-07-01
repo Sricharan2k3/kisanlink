bffa41e2e546678b4229eb2eb6580971eed3e160
Sricharan2k3 <ugs21117_cse.charan@cbit.org.in>
2024-07-01 12:32:04 +0530

    updated courses page

--- src/app/courses-and-certificates/page.js
+++ src/app/courses-and-certificates/page.js
@@ -1,11 +1,26 @@
 "use client";
-import React from "react";
+import React,{useState} from "react";
 import Heading from "../components/heading";
 import Background from "../../utils/background";
 import { FaYoutube } from "react-icons/fa6";
 import Link from "next/link";
 
 const ast = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  // Filtered links based on selected category
+  const filteredLinks = selectedCategory === "All" 
+    ? YouTubeLinks 
+    : YouTubeLinks.filter(link => link.category === selectedCategory);
+
+
+
   return (
     <>
       <Background title={"Agriculture Skill Training"}></Background>
@@ -84,160 +99,70 @@ const ast = () => {
           in fields of their interest to further their careers.
         </p>
       </div>
+      <div className="bg-white ">
+      <div className="w-full top-20 right-4 z-10">
+  <div className="bg-white bg-opacity-70 backdrop-blur-lg rounded-md  p-4 flex items-center justify-center">
+    <p className="text-gray-600 text-sm mb-1 mr-2">Filter by category:</p>
+    <button
+      onClick={() => handleCategoryClick("All")}
+      className={`mr-2 px-4 py-2 rounded-md ${
+        selectedCategory === "All"
+          ? "bg-blue-500 text-white"
+          : "bg-gray-200 text-gray-800"
+      }`}
+    >
+      All
+    </button>
+    <button
+      onClick={() => handleCategoryClick("English")}
+      className={`mr-2 px-4 py-2 rounded-md ${
+        selectedCategory === "English"
+          ? "bg-blue-500 text-white"
+          : "bg-gray-200 text-gray-800"
+      }`}
+    >
+      English
+    </button>
+    <button
+      onClick={() => handleCategoryClick("Telugu")}
+      className={`mr-2 px-4 py-2 rounded-md ${
+        selectedCategory === "Telugu"
+          ? "bg-blue-500 text-white"
+          : "bg-gray-200 text-gray-800"
+      }`}
+    >
+      Telugu
+    </button>
+    {/* Add more buttons for other categories */}
+  </div>
+</div>
 
-      {/* Image Gallery */}
-      <div className="grid grid-cols-2 gap-4 mt-10 mx-40">
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPsZfEIuwprDf937Fe0f7T3S"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/introductory-english-thumbnail.jpg"
-            alt="Agriculture Image 1"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/introductory-telugu-thumbnail.jpg"
-            alt="Agriculture image 2"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuJQuSTkqEd1qOdho6x_MPH"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/introductory-hindi-thumbnail.jpg"
-            alt="Agriculture Image 3"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuZ1bcWlPwrx37erWB77Mn0"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/eng.jpg"
-            alt="Agriculture Image 4"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/tel.jpg"
-            alt="Agriculture Image 5"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/hin.jpg"
-            alt="Agriculture Image 6"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPs9NBIGTN9mABTBpjsGFBJm"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/advisory-english-1.jpg"
-            alt="Agriculture Image 7"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/introductory-telugu-thumbnail.jpg"
-            alt="Agriculture Image 8"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/introductory-hindi-thumbnail.jpg"
-            alt="Agriculture Image 9"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/mustard-english-thumbnail.jpg"
-            alt="Agriculture Image 10"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/mustard-telugu-1-thumbnail.jpg"
-            alt="Agriculture Image 11"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/mustard-telugu-thumbnail.jpg"
-            alt="Agriculture Image 12"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/safflower-course-english.jpg"
-            alt="Agriculture Image 13"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS"
-          target="_blank"
-        >
-          <img
-            src="https://www.kisanlink.in/wp-content/uploads/safflower-course-hin.jpg"
-            alt="Agriculture Image 14"
-            className="w-full h-auto rounded-xl"
-          />
-        </a>
+
+      {/* Render YouTube Links */}
+      <div className=" bg-white grid grid-cols-2 gap-4 mt-10 mx-40">
+        {filteredLinks.map((link, index) => (
+          <a key={index} href={link.href} target="_blank">
+            <img
+              src={link.thumbnail}
+              alt={link.alt}
+              className=" bg-white w-full h-auto rounded-xl"
+            />
+          </a>
+        ))}
       </div>
 
-      {/* Centered Last Image */}
-      <div className="flex justify-center mt-10">
-        <a></a>
+      {/* Bottom Image */}
+      <div className="flex justify-center mt-12 pt-12">
         <img
-          src="https://www.kisanlink.in/wp-content/uploads/safflower-course-telugu.jpg"
-          alt="Agriculture Image 15"
-          className="w-2/5 h-2/5 rounded-xl"
+          src="https://www.kisanlink.in/wp-content/uploads/2022/07/agriculture-ge5750e9ae_1280-removebg-e1657976152581.png"
+          alt="Bottom Image"
+          className="w-full h-48"
         />
       </div>
+      </div>
+
+      
+         
 
       <div className="flex justify-center mt-20">
         <div className="-mt-24 pr-8">
@@ -400,3 +325,94 @@ const ast = () => {
 };
 
 export default ast;
+
+
+
+const YouTubeLinks = [
+  {
+    category: "English",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPsZfEIuwprDf937Fe0f7T3S",
+    alt: "Agriculture Image 1",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/introductory-english-thumbnail.jpg"
+  },
+  {
+    category: "Telugu",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture image 2",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/introductory-telugu-thumbnail.jpg"
+  },
+  {
+    category: "Hindi",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuJQuSTkqEd1qOdho6x_MPH",
+    alt: "Agriculture Image 3",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/introductory-hindi-thumbnail.jpg"
+  },
+  {
+    category: "English",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuZ1bcWlPwrx37erWB77Mn0",
+    alt: "Agriculture Image 4",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/eng.jpg"
+  },
+  {
+    category: "Telugu",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 5",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/tel.jpg"
+  },
+  {
+    category: "Hindi",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 6",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/hin.jpg"
+  },
+  {
+    category: "English",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPs9NBIGTN9mABTBpjsGFBJm",
+    alt: "Agriculture Image 7",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/advisory-english-1.jpg"
+  },
+  {
+    category: "Telugu",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 8",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/introductory-telugu-thumbnail.jpg"
+  },
+  {
+    category: "Hindi",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 9",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/introductory-hindi-thumbnail.jpg"
+  },
+  {
+    category: "English",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 10",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/mustard-english-thumbnail.jpg"
+  },
+  {
+    category: "Telugu",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 11",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/mustard-telugu-1-thumbnail.jpg"
+  },
+  {
+    category: "Telugu",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 12",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/mustard-telugu-thumbnail.jpg"
+  },
+  {
+    category: "English",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 13",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/safflower-course-english.jpg"
+  },
+  {
+    category: "Hindi",
+    href: "https://www.youtube.com/playlist?list=PL3UDtQYVvuPuGG1VociCoELxx_zJWYWHS",
+    alt: "Agriculture Image 14",
+    thumbnail: "https://www.kisanlink.in/wp-content/uploads/safflower-course-hin.jpg"
+  }
+];
+
+
