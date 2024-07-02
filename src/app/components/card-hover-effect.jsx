c446f322c17deefb3e8309a3638f4cb1db03c1bd
Sricharan2k3 <ugs21117_cse.charan@cbit.org.in>
2024-07-02 13:09:00 +0530

    updated experience center

--- src/app/components/card-hover-effect.jsx
+++ src/app/components/card-hover-effect.jsx
@@ -2,47 +2,40 @@ import { cn } from "@/utils/cn";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSeedling, faBroadcastTower, faTools } from "@fortawesome/free-solid-svg-icons";
 
-export const HoverEffect = ({
-  items,
-  className,
-}) => {
-  let [hoveredIndex, setHoveredIndex] = useState(null);
+export const HoverEffect = ({ items, className }) => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const icons = [faSeedling, faBroadcastTower, faTools]; // Example icons corresponding to each item
 
   return (
-    <div
-      className={cn(
-        "grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10",
-        className
-      )}
-    >
+    <div className={cn("grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10", className)}>
       {items.map((item, idx) => (
         <Link
           href="/"
           key={idx}
-          className="relative group  block p-2 h-full w-full"
+          className="relative group block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
         >
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
+                animate={{ opacity: 1, transition: { duration: 0.15 } }}
+                exit={{ opacity: 0, transition: { duration: 0.15, delay: 0.2 } }}
               />
             )}
           </AnimatePresence>
           <Card>
-            <CardTitle>{item.title}</CardTitle>
+            <div className="flex items-center mb-2">
+              <FontAwesomeIcon icon={icons[(idx % icons.length)]} className="text-green-500 mr-2 h-8 ml-2" />
+              <CardTitle>{item.title}</CardTitle>
+            </div>
             <CardDescription>{item.description}</CardDescription>
           </Card>
         </Link>
@@ -51,44 +44,27 @@ export const HoverEffect = ({
   );
 };
 
-export const Card = ({
-  className,
-  children,
-}) => {
+export const Card = ({ className, children }) => {
   return (
-    <div
-      className={cn(
-        "rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
-        className
-      )}
-    >
+    <div className={cn("rounded-2xl h-full w-full p-4 overflow-hidden bg-black border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 relative z-20", className)}>
       <div className="relative z-50">
         <div className="p-4">{children}</div>
       </div>
     </div>
   );
 };
-export const CardTitle = ({
-  className,
-  children,
-}) => {
+
+export const CardTitle = ({ className, children }) => {
   return (
     <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>
       {children}
     </h4>
   );
 };
-export const CardDescription = ({
-  className,
-  children,
-}) => {
+
+export const CardDescription = ({ className, children }) => {
   return (
-    <p
-      className={cn(
-        "mt-8 w-full text-zinc-400 tracking-wide leading-relaxed text-sm",
-        className
-      )}
-    >
+    <p className={cn("mt-8 w-full text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>
       {children}
     </p>
   );
