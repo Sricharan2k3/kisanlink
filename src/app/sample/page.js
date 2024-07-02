a9540a9f6e46d39bed739d3a5a333211c8b198a4
Sricharan2k3 <ugs21117_cse.charan@cbit.org.in>
2024-07-02 15:55:31 +0530

    updated sample navbar color

--- src/app/sample/page.js
+++ src/app/sample/page.js
@@ -3,7 +3,7 @@ import LogoComponent from "./logo";
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center  px-4">
+    <div className="flex justify-center items-center  px-4 bg-green-200">
       <div className="flex justify-between items-center max-w-screen-2xl w-full">
         <LogoComponent />
         <NavBarComponent />
