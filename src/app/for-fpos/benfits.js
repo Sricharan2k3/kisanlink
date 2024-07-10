bab9ede60d62e059f9fbc2dae86d48e5d12fd20a
Akash120903 <akashpingali18@gmail.com>
2024-07-10 12:41:34 +0530

    test

--- src/app/for-fpos/benfits.js
+++ src/app/for-fpos/benfits.js
@@ -6,7 +6,7 @@ const Card = ({ dancer, image, title, description, isImageLeft }) => {
   return (
     <div className="flex flex-row w-9/12 items-center con rounded-lg p-2 -mt-16">
       {isImageLeft && (
-        <div className=" flex flex-row w-72 h-72 rounded-lg overflow-hidden mr-4">
+        <div className=" flex flex-row  w-72 h-72 rounded-lg overflow-hidden mr-4">
           <img
             src={image}
             alt="Card Image"
