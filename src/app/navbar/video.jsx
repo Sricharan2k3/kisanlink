07f34f490e36ad4e0e5160ad3b7d3602c4291cc7
Sricharan2k3 <ugs21117_cse.charan@cbit.org.in>
2024-07-12 12:13:38 +0530

    sass mobile

--- src/app/navbar/video.jsx
+++ src/app/navbar/video.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 function VideoContainer() {
   return (
     <div className=" pt-20 flex items-center bg-white justify-center lg:h-screen -m-1.5 overflow-hidden">
-      <div className="relative overflow-hidden lg:w-full lg:h-full small:w-[600px] small:h-[450px] bg-white">
+      <div className="relative overflow-hidden lg:w-full lg:h-full small:w-full small:h-[450px] bg-white">
         <iframe
-          className="absolute overflow-hidden  lg:w-full lg:h-full small:w-[600px] small:h-[300px] "
+          className="absolute overflow-hidden  lg:w-full lg:h-full small:w-fulll small:h-[450px] "
           src="https://www.kisanlink.in/wp-content/uploads/pitchdesk_0.mp4"
           title="Full Screen Video"
          
