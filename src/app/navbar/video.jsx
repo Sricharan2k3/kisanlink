767f19cbfc773c974be72971421305fc658b7147
Akash120903 <akashpingali18@gmail.com>
2024-08-08 18:13:20 +0530

    video change

--- src/app/navbar/video.jsx
+++ src/app/navbar/video.jsx
@@ -15,8 +15,7 @@ function VideoContainer() {
       <div className="relative overflow-hidden lg:w-full lg:h-full small:w-full small:h-[280px] bg-white">
         <iframe
           className="absolute overflow-hidden  lg:w-full lg:h-full small:w-full small:h-[280px] "
-   src="https://d33hqsk72xx8w2.cloudfront.net/wp-content/uploads/new-admin-panel-1.mp4"
-          title="Full Screen Video"
+src='https://d33hqsk72xx8w2.cloudfront.net/wp-content/uploads/new-admin-panel-1.mp4'          title="Full Screen Video"
          
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
