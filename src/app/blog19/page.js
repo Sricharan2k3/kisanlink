cc36407e52bfabb84c7bc081c5b04390f110fd4c
Akash120903 <akashpingali18@gmail.com>
2024-09-06 05:10:54 +0530

    blog19_modified

--- src/app/blog19/page.js
+++ src/app/blog19/page.js
@@ -10,17 +10,17 @@ import Background from "../../utils/background";
 
 const References = () => {
   const referencesList = [
-    "Economic Times. (2024). AgriSURE fund launched by Union Minister Shivraj Singh Chouhan: Here’s all you need to know about the initiative. Retrieved from https://economictimes.indiatimes.com/news/economy/agriculture/agrisure-fund-launched-by-union-minister-shivraj-singh-chouhan-heres-all-you-need-to-know-about-the-initiative/articleshow/113023200.cms",
-    "NABARD. (2024). NABARD announces ₹750 crore ‘Agri SURE’ fund to promote innovation in agriculture. Retrieved from https://www.thehindu.com/business/nabard-announces-750-crore-agri-sure-fund-to-promote-innovation-in-agriculture/article68396955.ece",
+    "Economic Times. (2024). AgriSURE fund launched by Union Minister Shivraj Singh Chouhan: Here's all you need to know about the initiative. Retrieved from https://economictimes.indiatimes.com/news/economy/agriculture/agrisure-fund-launched-by-union-minister-shivraj-singh-chouhan-heres-all-you-need-to-know-about-the-initiative/articleshow/113023200.cms",
+    "NABARD. (2024). NABARD announces ₹750 crore 'Agri SURE' fund to promote innovation in agriculture. Retrieved from https://www.thehindu.com/business/nabard-announces-750-crore-agri-sure-fund-to-promote-innovation-in-agriculture/article68396955.ece",
     "Inc42. (2024). Govt Debuts INR 750 Cr AgriSURE Fund To Back Agritech Startups. Retrieved from https://inc42.com/buzz/centre-launches-inr-750-cr-agrisure-fund-to-back-agritech-startups/",
   ];
 
   return (
-    <div className="mt-4">
-      <h2 className="text-xl text-center font-bold mb-4">References</h2>
-      <ul className="space-y-2">
+    <div className="mt-8">
+      <h2 className="text-2xl font-bold mb-4">References</h2>
+      <ul className="space-y-4">
         {referencesList.map((ref, index) => (
-          <li key={index} className="text-md">
+          <li key={index} className="text-sm">
             {ref}
           </li>
         ))}
@@ -43,49 +43,50 @@ export default function Component() {
       <div className="flex flex-col lg:flex-row w-full mx-auto px-4 sm:px-6 lg:px-8 mt-8">
         <div className="lg:w-full lg:pr-8">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-            <article className="prose prose-gray max-w-none pl-6 pr-6 pt-2">
+            <article className="prose prose-gray max-w-none p-8">
               <SubheadingDescription text="The Indian agricultural landscape is poised for a significant transformation with the recent launch of the AgriSURE Fund and the Krishi Nivesh Portal by Union Minister Shivraj Singh Chouhan. These initiatives, aimed at fostering innovation and enhancing investment in agriculture, reflect the government's commitment to empowering farmers and promoting sustainable agritech solutions." />
 
-              <SubheadingDescription
-                subheading="AgriSURE Fund: A Catalyst for Innovation"
-                text="The AgriSURE Fund, officially launched on September 3, 2024, is a ₹750 crore (approximately $90 million) initiative designed to support technology-driven startups and rural enterprises. This fund is structured as a Category-II Alternative Investment Fund (AIF) registered with the Securities and Exchange Board of India (SEBI). It aims to provide equity and debt funding to approximately 85 startups, with individual investments capped at ₹25 crore each."
-              />
-              <br></br>
-              <ul className="list-disc space-y-4 list-inside">
-                <li>
-                  Financial Contributions: The fund comprises ₹250 crore from
-                  the Government of India, ₹250 crore from the National Bank for
-                  Agriculture and Rural Development (NABARD), and ₹250 crore
-                  mobilized from banks, insurance companies, and private
-                  investors.
-                </li>
-                <li>
-                  Focus Areas: It targets high-risk, high-impact activities
-                  across the agricultural value chain, including enhancing farm
-                  produce value chains, creating rural infrastructure, and
-                  supporting Farmer Producer Organizations (FPOs).
-                </li>
-                <li>
-                  Support for Innovation: The fund will encourage the
-                  development of IT-based solutions, machinery rental services,
-                  and other innovative approaches to address farmers'
-                  challenges, thereby fostering a sustainable agricultural
-                  ecosystem.
-                </li>
-              </ul>
-              <br></br>
-              <img
-                src="https://d33hqsk72xx8w2.cloudfront.net/wp-content/uploads/AGRISURE-ILLUSTRATION.png"
-                alt="Farmer Producer Organizations"
-                className="ml-[500px] w-[400px] h-[400px]"
-              />
-              <br></br>
+              <div className="flex flex-col md:flex-row items-start gap-8 mt-8">
+                <img
+                  src="https://d33hqsk72xx8w2.cloudfront.net/wp-content/uploads/AGRISURE-ILLUSTRATION.png"
+                  alt="Farmer Producer Organizations"
+                  className="w-full md:w-1/3 h-auto object-cover rounded-lg"
+                />
+                <div>
+                  <SubheadingDescription
+                    subheading="AgriSURE Fund: A Catalyst for Innovation"
+                    text="The AgriSURE Fund, officially launched on September 3, 2024, is a ₹750 crore (approximately $90 million) initiative designed to support technology-driven startups and rural enterprises. This fund is structured as a Category-II Alternative Investment Fund (AIF) registered with the Securities and Exchange Board of India (SEBI). It aims to provide equity and debt funding to approximately 85 startups, with individual investments capped at ₹25 crore each."
+                  />
+                  <ul className="list-disc space-y-4 list-inside mt-4">
+                    <li>
+                      Financial Contributions: The fund comprises ₹250 crore
+                      from the Government of India, ₹250 crore from the National
+                      Bank for Agriculture and Rural Development (NABARD), and
+                      ₹250 crore mobilized from banks, insurance companies, and
+                      private investors.
+                    </li>
+                    <li>
+                      Focus Areas: It targets high-risk, high-impact activities
+                      across the agricultural value chain, including enhancing
+                      farm produce value chains, creating rural infrastructure,
+                      and supporting Farmer Producer Organizations (FPOs).
+                    </li>
+                    <li>
+                      Support for Innovation: The fund will encourage the
+                      development of IT-based solutions, machinery rental
+                      services, and other innovative approaches to address
+                      farmers' challenges, thereby fostering a sustainable
+                      agricultural ecosystem.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
               <SubheadingDescription
                 subheading="Eligibility Criteria for AgriSURE Fund"
                 text=""
               />
-              <br />
-              <ul className="list-disc space-y-4 list-inside">
+              <ul className="list-disc space-y-4 list-inside ">
                 <li>
                   Sector Focus: Startups must operate within the agriculture or
                   allied sectors. This includes businesses that enhance the
@@ -122,7 +123,7 @@ export default function Component() {
                 subheading="Krishi Nivesh Portal: Centralizing Agricultural Investment"
                 text="Alongside the AgriSURE Fund, the Krishi Nivesh Portal was also launched to centralize investment opportunities and information related to agribusinesses. This integrated platform aims to enhance farmers' and agritech startups' access to resources, ultimately boosting their income and productivity."
               />
-              <ul className="list-disc space-y-4 list-inside">
+              <ul className="list-disc space-y-4 list-inside mt-4">
                 <li>
                   Enhance Visibility: Providing a one-stop solution for
                   agribusinesses to showcase their investment needs and
@@ -143,18 +144,11 @@ export default function Component() {
 
               <References />
             </article>
-            <div className="p-6">
+            <div className="p-8">
               <CommentForm />
             </div>
           </div>
         </div>
-        {/* <div className="lg:w-1/3 mt-8 lg:mt-0">
-          <div className="bg-white shadow-lg rounded-lg p-6 space-y-6">
-            <CategoryList />
-            <TagList />
-            <Footer />
-          </div>
-        </div> */}
       </div>
     </>
   );
